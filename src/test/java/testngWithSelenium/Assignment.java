062570294f21be266ba4c640a54a19c5d36a2678
Nistha Jain <nisjain4@publicisgroupe.net>
2022-09-13 18:01:21 +0530

    Implicit and Explicit waits added, xpath updated

--- src/test/java/testngWithSelenium/Assignment.java
+++ src/test/java/testngWithSelenium/Assignment.java
@@ -1,10 +1,14 @@
 package testngWithSelenium;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -22,17 +26,20 @@ public class Assignment {
 		driver.get("http://www.amazon.in");
 		
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		WebDriverWait driverWait=new WebDriverWait(driver, Duration.ofSeconds(10));
 		
 		JavascriptExecutor js=(JavascriptExecutor)driver;
 		
-		String title=driver.getTitle();
+		//String title=driver.getTitle();
 		
-		driver.findElement(By.xpath("//*[@id=\"nav-hamburger-menu\"]")).click();
-		Thread.sleep(1000);
+		driver.findElement(By.id("nav-hamburger-menu")).click();
+		//Thread.sleep(1000);
 		
 		WebElement text=driver.findElement(By.xpath("//*[@id=\"hmenu-content\"]/ul[1]/li[14]/div"));
 		js.executeScript("arguments[0].scrollIntoView();",text);
-		Thread.sleep(1000);
+		//Thread.sleep(1000);
 		
 		driver.findElement(By.xpath("//*[@id=\"hmenu-content\"]/ul[1]/li[16]/a")).click();
 		Thread.sleep(1000);
@@ -40,21 +47,22 @@ public class Assignment {
 		driver.findElement(By.linkText("Televisions")).click();  
 		Thread.sleep(1000);
 		
-		WebElement category=driver.findElement(By.xpath("//*[@id=\"s-refinements\"]/div[21]/div"));
+		driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='a-size-base a-color-base a-text-bold' and text()='Brands']")));
+		WebElement category=driver.findElement(By.xpath("//span[@class='a-size-base a-color-base a-text-bold' and text()='Brands']"));
 		js.executeScript("arguments[0].scrollIntoView();",category);
-		Thread.sleep(1000);
+		//Thread.sleep(1000);
 		
-		driver.findElement(By.xpath("//*[@id=\"s-refinements\"]/div[21]/ul/li[5]/span/a/div")).click();
-		Thread.sleep(1000);
+		driver.findElement(By.xpath("//span[@class='a-size-base a-color-base' and text()='Samsung']")).click();
+		//Thread.sleep(1000);
 		
 		driver.findElement(By.xpath("//*[@id=\"a-autoid-0-announce\"]/span[2]")).click(); 
-		Thread.sleep(1000);
+		//Thread.sleep(1000);
 		
 		driver.findElement(By.id("s-result-sort-select_2")).click(); 
-		Thread.sleep(1000);
+		//Thread.sleep(1000);
 		
 		driver.findElement(By.xpath("//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div[3]")).click(); 
-		Thread.sleep(1000);
+		//Thread.sleep(1000);
 		
 		
 		for(String winHandle : driver.getWindowHandles()){
@@ -63,7 +71,7 @@ public class Assignment {
 		
 		WebElement about=driver.findElement(By.id("feature-bullets"));
 		js.executeScript("arguments[0].scrollIntoView();",about);
-		Thread.sleep(2000);
+		//Thread.sleep(2000);
 		
 		Assert.assertEquals(true, about.isDisplayed());
 		
